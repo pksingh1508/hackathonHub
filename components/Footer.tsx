2dc729e76939e39a841556d194517f83431056be
PawanKumar <rajiit1212@gmail.com>
2024-10-26 09:31:09 +0530

    fix type in website name in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -57,7 +57,7 @@ const Footer = () => {
                 </div>
 
                 <div className="mt-8 pt-8 border-t border-gray-700 text-center text-gray-400">
-                    <p>&copy; {new Date().getFullYear()} HackathonHub. All rights reserved.</p>
+                    <p>&copy; {new Date().getFullYear()} HackathonWallah. All rights reserved.</p>
                 </div>
             </div>
         </footer>
